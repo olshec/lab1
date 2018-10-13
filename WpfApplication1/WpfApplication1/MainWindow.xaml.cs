67f40555162bce97773ccdb186b24195e2aaa787
olshec <olshec@gmail.com>
2018-10-13 09:12:17 +0300

    add reg class

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -40,147 +40,148 @@ namespace WpfApplication1
 
        
 
-        string[] masNameTypeWithNull = new string[] {"sbyte","short","int","long","byte","ushort","uint",
-               "ulong","float","double","decimal","bool","char"};
+        //string[] masNameTypeWithNull = new string[] {"sbyte","short","int","long","byte","ushort","uint",
+        //       "ulong","float","double","decimal","bool","char"};
 
-        string[] masNameTypeNotNull = new string[] { "string", "object" };
+        //string[] masNameTypeNotNull = new string[] { "string", "object" };
 
-        string[] masNameAllType = new string[] {"sbyte","short","int","long","byte","ushort","uint",
-               "ulong","float","double","decimal","bool","char", "string", "object" };
+        //string[] masNameAllType = new string[] {"sbyte","short","int","long","byte","ushort","uint",
+        //       "ulong","float","double","decimal","bool","char", "string", "object" };
 
-        int checkString(string query, List<string> listVars)
-        {
-
-
-
-            string nameTypeWithNull = String.Join("|", masNameTypeWithNull);
-            string nameTypeNotNull = String.Join("|", masNameTypeNotNull);
-            string nameAllType = String.Join("|", masNameAllType);
+        //int checkString(string query, List<string> listVars)
+        //{
 
 
-            string pattern = @"^
-            (
-                (
-                    (          (?<=^)                          (" + nameAllType + @")                    (?=\?|\[|\s)       )    |
-                    (          (?<=" + nameTypeWithNull + @")  (\?)                                      (?=\[|\w+)         )    |
-                    (          (?<=" + nameAllType + @")       (\s)                                      (?=\w+)            )    |
-                    (          (?<=\?|" + nameAllType + @")    (?<Sk>\[)                                 (?=,|\])           )    |
-                    (          (?<=\w+|\[)                     (,)                                       (?=,|\]|\w+)       )    |
-                    (          (?<=,)                          (?(Sk),)                                  (?=,|\])           )    |
-                    (          (?<=,|\[)                       (?<-Sk>\])                                (?=\w+)            )    |
-                    (          (?<=\?|\s|\]|,)  (?(Sk)(?!))    (?<NameVar>\w+)                           (?=,|;)            )    |
-                    (          (?<=\w+)         (?(Sk)(?!))    (;)                                       (?=;|$)            )    |
-                )+
-            )";
+
+        //    string nameTypeWithNull = String.Join("|", masNameTypeWithNull);
+        //    string nameTypeNotNull = String.Join("|", masNameTypeNotNull);
+        //    string nameAllType = String.Join("|", masNameAllType);
+
+
+        //    string pattern = @"^
+        //    (
+        //        (
+        //            (          (?<=^)                          (" + nameAllType + @")                    (?=\?|\[|\s)       )    |
+        //            (          (?<=" + nameTypeWithNull + @")  (\?)                                      (?=\[|\w+)         )    |
+        //            (          (?<=" + nameAllType + @")       (\s)                                      (?=\w+)            )    |
+        //            (          (?<=\?|" + nameAllType + @")    (?<Sk>\[)                                 (?=,|\])           )    |
+        //            (          (?<=\w+|\[)                     (,)                                       (?=,|\]|\w+)       )    |
+        //            (          (?<=,)                          (?(Sk),)                                  (?=,|\])           )    |
+        //            (          (?<=,|\[)                       (?<-Sk>\])                                (?=\w+)            )    |
+        //            (          (?<=\?|\s|\]|,)  (?(Sk)(?!))    (?<NameVar>\w+)                           (?=,|;)            )    |
+        //            (          (?<=\w+)         (?(Sk)(?!))    (;)                                       (?=;|$)            )    |
+        //        )+
+        //    )";
 
             
 
-            Regex r = new Regex(pattern, RegexOptions.IgnorePatternWhitespace);
+        //    Regex r = new Regex(pattern, RegexOptions.IgnorePatternWhitespace);
 
-            Match m = r.Match(query);
+        //    Match m = r.Match(query);
 
-            int lenthTrueQuery = query.Length;
-            if (m.Groups.Count > 1)
-            {
-                Group g = m.Groups[1];
+        //    int lenthTrueQuery = query.Length;
+        //    if (m.Groups.Count > 1)
+        //    {
+        //        Group g = m.Groups[1];
                 
-                richTextBox.AppendText(String.Format("True string: '{0}'   Length: '{1}'",
-                    g.Value, g.Length));
-                richTextBox.AppendText(Environment.NewLine + "QUWERY = " + query.Length + Environment.NewLine);
-                if (g.Value.Length < query.Length)
-                {
-                    if(g.Value.Length==0)
-                        lenthTrueQuery = 1;
-                    else
-                        if(g.Value.ToCharArray()[g.Value.Length-1]==' ')
-                            lenthTrueQuery = g.Value.Length + 3;
-                        else
-                            lenthTrueQuery = g.Value.Length + 2;
-                }
-                string nameGroup = "NameVar";
-                g = m.Groups[nameGroup];
-                for (int j = 0; j < g.Captures.Count; j++)
-                {
-                    Capture c = g.Captures[j];
-                    listVars.Add(c.Value);
-                }
+        //        richTextBox.AppendText(String.Format("True string: '{0}'   Length: '{1}'",
+        //            g.Value, g.Length));
+        //        richTextBox.AppendText(Environment.NewLine + "QUWERY = " + query.Length + Environment.NewLine);
+        //        if (g.Value.Length < query.Length)
+        //        {
+        //            if(g.Value.Length==0)
+        //                lenthTrueQuery = 1;
+        //            else
+        //               // if(g.Value.ToCharArray()[g.Value.Length-1]==' ')
+        //               //     lenthTrueQuery = g.Value.Length + 3;
+        //               // else
+        //                    lenthTrueQuery = g.Value.Length + 2;
+        //        }
+        //        string nameGroup = "NameVar";
+        //        g = m.Groups[nameGroup];
+        //        for (int j = 0; j < g.Captures.Count; j++)
+        //        {
+        //            Capture c = g.Captures[j];
+        //            listVars.Add(c.Value);
+        //        }
 
-               // richTextBox.AppendText("SKOPEN: ");
-                nameGroup = "Sk";
-                g = m.Groups[nameGroup];
-                for (int j = 0; j < g.Captures.Count; j++)
-                {
-                    Capture c = g.Captures[j];
-                    richTextBox.AppendText("SKOPEN: "+c.Value);
+        //       // richTextBox.AppendText("SKOPEN: ");
+        //        nameGroup = "Sk";
+        //        g = m.Groups[nameGroup];
+        //        for (int j = 0; j < g.Captures.Count; j++)
+        //        {
+        //            Capture c = g.Captures[j];
+        //            richTextBox.AppendText("SKOPEN: "+c.Value);
 
-                }
-                return lenthTrueQuery;
-            }
-            return lenthTrueQuery;
+        //        }
+        //        return lenthTrueQuery;
+        //    }
+        //    return lenthTrueQuery;
 
-        }
+        //}
 
-        bool checkKey(char chekChar)
-        {
-            char[] listReplaceString = new char[] { '?', '[', ']', ',', ';' };
-            foreach (char c in listReplaceString)
-            {
-                if (c == chekChar)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
+        //bool checkKey(char chekChar)
+        //{
+        //    char[] listReplaceString = new char[] { '?', '[', ']', ',', ';' };
+        //    foreach (char c in listReplaceString)
+        //    {
+        //        if (c == chekChar)
+        //        {
+        //            return true;
+        //        }
+        //    }
+        //    return false;
+        //}
 
-        void formatString(ref string str)
-        {
-            string[] listReplaceString = new string[] { "\n", "  " };
-            foreach (string s in listReplaceString)
-                while (str.IndexOf(s) != -1)
-                    str = str.Replace(s, " ");
-            while (str.IndexOf(";;") != -1)
-                str = str.Replace(";;", ";");
+        //void formatString(ref string str)
+        //{
+        //    string[] listReplaceString = new string[] { "\n", "  " };
+        //    foreach (string s in listReplaceString)
+        //        while (str.IndexOf(s) != -1)
+        //            str = str.Replace(s, " ");
+        //    while (str.IndexOf(";;") != -1)
+        //        str = str.Replace(";;", ";");
 
-            if (str.Length > 1)
-                if (str.ToCharArray()[0] == ' ')
-                    str = str.Remove(0, 1);
+        //    if (str.Length > 1)
+        //        if (str.ToCharArray()[0] == ' ')
+        //            str = str.Remove(0, 1);
 
-            if (str.Length > 1)
-                if (str.ToCharArray()[str.Length - 1] == ' ')
-                {
-                    str = str.Remove(str.Length - 1, 1);
-                }
+        //    if (str.Length > 1)
+        //        if (str.ToCharArray()[str.Length - 1] == ' ')
+        //        {
+        //            str = str.Remove(str.Length - 1, 1);
+        //        }
                    
 
-            if (str.Length > 0)
-            {
-                string[] masString = str.Split(' ');
-                string newString = "";
-                newString += masString[0];
-                for (int i = 0; i < masString.Length - 1; i++)
-                {
+        //    if (str.Length > 0)
+        //    {
+        //        string[] masString = str.Split(' ');
+        //        string newString = "";
+        //        newString += masString[0];
+        //        for (int i = 0; i < masString.Length - 1; i++)
+        //        {
 
-                    if (!checkKey(masString[i].ToCharArray()[masString[i].Length - 1]) &&
-                        !checkKey(masString[i + 1].ToCharArray()[0]))
-                        newString += " ";
-                    newString += masString[i + 1];
-                }
-                str = newString;
-            }
-            str += "";
+        //            if (!checkKey(masString[i].ToCharArray()[masString[i].Length - 1]) &&
+        //                !checkKey(masString[i + 1].ToCharArray()[0]))
+        //                newString += " ";
+        //            newString += masString[i + 1];
+        //        }
+        //        str = newString;
+        //    }
+        //    str += "";
 
-        }
+        //}
 
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
+            RegAnalisator ra = new RegAnalisator();
             List<string> listVars = new List<string>();
-            float?[,,,] a, b; ; ; ;
+            //float?[,,,] a, b; ; ; ;
             richTextBox.Document.Blocks.Clear();
 
-            string query = "     string [, ,,] bbb, a2  , uu ;;; float a ff ;; ";
-            formatString(ref query);
+            string query = textBox.Text;
+            ra.formatString(ref query);
 
             int positionError = -1;
             int indexQuery = -1;
@@ -191,8 +192,14 @@ namespace WpfApplication1
                 countString--;
             for (int i=0;i< countString; i++)
             {
-                string q = masStr[i]+";" ;
-                int lenth = checkString(q, listVars);
+                string q = masStr[i];
+                if (i == countString - 1)
+                {
+                    if (query.ToCharArray()[query.Length - 1] == ';')
+                        q += ";";
+                }
+                else q += ";" ;
+                int lenth = ra.checkString(q, listVars);
                 if (q.Length>lenth)
                 {
                     positionError = lenth;
