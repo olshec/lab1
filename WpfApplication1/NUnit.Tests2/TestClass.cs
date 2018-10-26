df9aea7b785c3324d51a5eff601d001e03fc6969
olshec <olshec@gmail.com>
2018-10-26 09:47:19 +0300

    change tests

--- WpfApplication1/NUnit.Tests2/TestClass.cs
+++ WpfApplication1/NUnit.Tests2/TestClass.cs
@@ -12,72 +12,80 @@ namespace NUnit.Tests2
     [TestFixture]
     public class TestClass
     {
-        //begin tests extract symbol
-        [TestCase("int ! , a;",'!')]
-        [TestCase("int !b , a;", '!')]
-        [TestCase("int b! , a;", '!')]
-        [TestCase("int a, b^ ,c;", '^')]
-        //end tests extract symbol
 
-        //begin tests without type
-        [TestCase("dd , a;", 'd')]
-        [TestCase("int b; dd , a;", 'd')]
-        [TestCase("int j, b d;", 'd')]
-        [TestCase("int j; b d;", 'b')]
+        static object[] DivideCases =
+        {
+            //begin tests extract symbol
+            new object[] { "int ! , a;",'!',true},
+            new object[] { "int !b , a;", '!',true},
+            new object[] { "int b! , a;", '!',true},
+            new object[] { "int a, b^ ,c;", '^',true},
+            //end tests extract symbol
+
+            //begin tests without type
+            new object[] { "dd , a;", 'd',true},
+             new object[] {"int b; dd , a;", 'd',true},
+        new object[] {"int j, b d;", 'd',true},
+        new object[] {"int j; b d;", 'b',true},
         //end tests without type
 
         //begin tests without symbol ';'
-        [TestCase("int j, d", ';')]
+        new object[] {"int j, d", ';',true},
         //end tests without symbol ';'
 
         //begin tests without symbol ','
-        [TestCase("int j; int b d;", 'd')]
+        new object[] {"int j; int b d;", 'd',true},
         //end tests without symbol ','
 
-        
-        [TestCase("float?[,,,] b1!,a2,v3,b1;", '!')]
-        [TestCase("float !?[,,,] b1,a2,v3,b1;", '!')]
-        [TestCase("float?![,,,] b1,a2,v3,b1;", '!')]
-        [TestCase("float?[,!,,] b1,a2,v3,b1;", '!')]
 
-        [TestCase("string? b1,a2,v3,b1;", '?')]
-        [TestCase("object?[] b1,a2,v3,b1;", '?')]
+        new object[] {"float?[,,,] b1!,a2,v3,b1;", '!',true},
+        new object[] {"float !?[,,,] b1,a2,v3,b1;", '!',true},
+        new object[] {"float?![,,,] b1,a2,v3,b1;", '!',true},
+        new object[] {"float?[,!,,] b1,a2,v3,b1;", '!',true},
 
-
-        [TestCase("int a,b,c,a,d;", 'a')]
-        [TestCase("string b1,a2,v3,a2;", 'a')]
-        [TestCase("float?[,,,] b1,a2,v3,b1;", 'b')]
-        [TestCase("int a,b; string c,a,d;", 'a')]
-        [TestCase("string b1,a2; string v3,a2;", 'a')]
-        [TestCase("float?[,,,] b1,a2,v3; string b1;", 'b')]
+        new object[] {"string? b1,a2,v3,b1;", '?',true},
+        new object[] {"object?[] b1,a2,v3,b1;", '?',true},
 
 
-        public void Test_GetErrorSymbol(string query, char errorSymbol)
+        new object[] {"int a,b,c,a,d;", 'a',true},
+        new object[] {"string b1,a2,v3,a2;", 'a',true},
+        new object[] {"float?[,,,] b1,a2,v3,b1;", 'b',true},
+        new object[] {"int a,b; string c,a,d;", 'a',true},
+        new object[] {"string b1,a2; string v3,a2;", 'a',true},
+        new object[] {"float?[,,,] b1,a2,v3; string b1;", 'b',true},
+
+
+        new object[] {"string b1,a2,v3;", ';' , false},
+        new object[] {"object b1,a2,v3;", ';' , false},
+        new object[] {"string[,,,] b1,a2,v3;", ';' , false},
+        new object[] {"object[,,,] b1,a2,v3;", ';' , false},
+        new object[] {"int[,,,] b1,a2,v3;", ';' , false},
+        new object[] {"bool[,,,] b1,a2,v3;", ';' , false},
+        new object[] {"int?[,,,] b1,a2,v3;", ';' , false},
+        new object[] {"bool?[,,,] b1,a2,v3;", ';' , false},
+        new object[] {"int? b1,a2,v3;", ';' , false},
+        new object[] {"bool? b1,a2,v3;", ';' , false},
+
+            };
+
+
+
+        [Test, TestCaseSource("DivideCases")]
+        public void Test_GetErrorSymbol(string query, char errorSymbol, bool hasError)
         {
 
             RegAnalisator ra = new RegAnalisator();
             List<string> listVars = new List<string>();
             List<string> listTypes = new List<string>();
 
-            InfoAboutError inf = ra.getTrueQuery(query,listVars, listTypes);
+            InfoAboutError inf = ra.getTrueQuery(query, listVars, listTypes);
 
             Assert.AreEqual(inf.errorChar, errorSymbol);
         }
 
 
-
-
-        [TestCase("string b1,a2,v3;", false)]
-        [TestCase("object b1,a2,v3;", false)]
-        [TestCase("string[,,,] b1,a2,v3;", false)]
-        [TestCase("object[,,,] b1,a2,v3;", false)]
-        [TestCase("int[,,,] b1,a2,v3;", false)]
-        [TestCase("bool[,,,] b1,a2,v3;", false)]
-        [TestCase("int?[,,,] b1,a2,v3;", false)]
-        [TestCase("bool?[,,,] b1,a2,v3;", false)]
-        [TestCase("int? b1,a2,v3;", false)]
-        [TestCase("bool? b1,a2,v3;", false)]
-        public void Test_QueryWithoutError(string query, bool hasError)
+        [Test, TestCaseSource("DivideCases")]
+        public void Test_QueryWithoutError(string query, char errorSymbol, bool hasError)
         {
 
             RegAnalisator ra = new RegAnalisator();
